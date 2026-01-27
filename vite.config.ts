3f2d1a6e21fb46b0262e9c85293e4cc0218475f7
Trevor B <trevorjbaron@gmail.com>
2026-01-26 22:08:32 -0800

    try avoid script defer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import * as staticCopy from 'vite-plugin-static-copy'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [tailwindcss(), react()],
+  plugins: [staticCopy.viteStaticCopy({
+    targets: [{
+      src: 'src/script.js',
+      dest: 'assets'
+    }]
+  }), tailwindcss(), react()],
 })
